a132f932407dc022936c22b942682c0d44c9923e
yany <nguyenthanhtoanx2@gmail.com>
2023-09-09 02:06:42 +0700

    Add project files.

---
+++ BookStore/Controllers/NguoiDungController.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using BookStore.Models;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BookStore.Controllers
+{
+    public class NguoiDungController : Controller
+    {
+        QuanLiBanSachEntities db = new QuanLiBanSachEntities();
+        // GET: NguoiDung
+        public ActionResult Index()
+        {
+            return View();
+        }
+        // Get DangKi 
+        public ActionResult DangKi()
+        {
+            return View();
+        }
+
+        //Get thong tin nguoi dung 
+        [HttpPost]
+      //  [ValidateAntiForgeryToken]
+        public ActionResult DangKi(KhachHang kh)
+        {//neu hop le thy cho them vao
+            if (ModelState.IsValid)
+            {
+                db.KhachHangs.Add(kh);
+                db.SaveChanges();
+            }
+
+            return View();
+        }
+        
+       // Get Dang Nhap
+        public ActionResult DangNhap()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult DangNhap(FormCollection f)
+        {
+            string tk = f["txtTaiKhoan"].ToString();
+            string mk = f["txtMatKhau"].ToString();
+            if (tk == "" || mk == "")
+            {
+                ViewBag.ThongBao = "Bạn Chưa Nhập Tài Khoản Hoặc Mật Khẩu.";
+                return View();
+            }
+
+            KhachHang kh = db.KhachHangs.SingleOrDefault(n => n.TaiKhoan == tk && n.MatKhau == mk);
+            if (kh != null)
+            {
+                ViewBag.ThongBao = "Chúc Mừng Bạn Đã Đăng Nhập Thành Công.";
+                Session["TaiKhoan"] = kh;
+                return RedirectToAction("Index", "Home");
+            }
+
+            ViewBag.ThongBao = "Tên Tài Khoản Hoặc Mật Khẩu Không ĐÚng.";
+            return View();
+        }
+    }
+}
---
+++ BookStore/Controllers/SachController.cs
@@ -0,0 +1,40 @@
+﻿using BookStore.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BookStore.Controllers
+{
+    public class SachController : Controller
+    {
+        QuanLiBanSachEntities db = new QuanLiBanSachEntities();
+        // GET: Sach
+        public ActionResult Index()
+        {
+            return View();
+        }
+        //List danh sach :
+        public ActionResult DanhsachSach()
+        {
+            return View(db.Saches.ToList());
+        }
+
+        public PartialViewResult SachMoiPartial()
+        {
+            var ListSachMoi = db.Saches.Take(3).ToList();
+            return PartialView(ListSachMoi);
+        }
+        public ViewResult XemChiTiet(int Ma = 0)
+        {
+            Sach sach = db.Saches.SingleOrDefault(n => n.MaSach == Ma);
+            if (sach == null)
+            {
+                Response.StatusCode = 404;
+                return null;
+            }
+            return View(sach);
+        }
+    }
+}
